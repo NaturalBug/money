a2a8d3492dc6b15315ca1eaeb62495bc349c6655
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-05-22 18:19:15 +0800

    style: correct Indentation

--- app/src/main/java/money/Franc.java
+++ app/src/main/java/money/Franc.java
@@ -1,18 +1,18 @@
 package money;
 
 public class Franc {
-	private int amount;
+    private int amount;
 
-	public Franc(int amount) {
-		this.amount = amount;
-	}
+    public Franc(int amount) {
+        this.amount = amount;
+    }
 
-	public Franc times(int multiplier) {
-		return new Franc(amount * multiplier);
-	}
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
 
-	public boolean equals(Object object) {
-		Franc franc = (Franc) object;
-		return amount == franc.amount;
-	}
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
 }

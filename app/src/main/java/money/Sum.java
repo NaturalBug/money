ee1ca5e858c008a324884cf92d3d1c2355fd9927
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-05-31 21:36:25 +0800

    test: use Sum to store augend and addend

---
+++ app/src/main/java/money/Sum.java
@@ -0,0 +1,11 @@
+package money;
+
+public class Sum implements Expression {
+    public Sum(Money augend, Money addend) {
+        this.augend = augend;
+        this.addend = addend;
+    }
+
+    public Money augend;
+    public Money addend;
+}

562fb746415e32fa56c6e57bfca6f3a51cbb2d1f
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-06-14 08:07:06 +0800

    test: add addRate and make it pass

--- app/src/main/java/money/Bank.java
+++ app/src/main/java/money/Bank.java
@@ -4,4 +4,7 @@ public class Bank {
     public Money reduce(Expression source, String to) {
         return source.reduce(to);
     }
+
+    public void addRate(String string, String string2, int i) {
+    }
 }

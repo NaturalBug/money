1f5a6cc32542848e123de2367a3d1ea69da82f69
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-06-14 08:17:55 +0800

    refactor: move rate into bank

--- app/src/main/java/money/Bank.java
+++ app/src/main/java/money/Bank.java
@@ -2,9 +2,13 @@ package money;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String string, String string2, int i) {
+    }
+
+    public int rate(String currency, String to) {
+        return currency.equals("CHF") && to.equals("USD") ? 2 : 1;
     }
 }

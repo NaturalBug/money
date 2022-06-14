9daaf0333d8ab468960a2736be4fecbb70e0a165
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-06-14 23:35:36 +0800

    fix: correct reduce of Sum

--- app/src/test/java/money/MoneyTest.java
+++ app/src/test/java/money/MoneyTest.java
@@ -74,4 +74,14 @@ class MoneyTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    public void testMixedAddition() {
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
